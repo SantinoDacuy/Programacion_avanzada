4eb007a9ce1d1cb317c06140019ee0ad30999972
Santino Dacuy <santinodacuy@gmail.com>
2025-08-29 11:16:19 -0300

    Segundo tp

---
+++ TP-2/E1.js
@@ -0,0 +1,9 @@
+const router = {
+    modelo: "AX5400",
+    marca: "TP-Link",
+    puertos: 4,
+    velocidad: 1000,
+    soportaWifi: true
+};
+
+console.log(router)
---
+++ TP-2/E2.js
@@ -0,0 +1,32 @@
+const dispositivosRed = [
+    {
+        tipo: "Router",
+        marca: "Cisco",
+        modelo: "ISR 4321",
+        precio: 1200
+    },
+    {
+        tipo: "Switch",
+        marca: "HP",
+        modelo: "Aruba 2930F",
+        precio: 800
+    },
+    {
+        tipo: "Firewall",
+        marca: "Fortinet",
+        modelo: "FortiGate 60F",
+        precio: 1500
+    },
+    {
+        tipo: "Access Point",
+        marca: "Ubiquiti",
+        modelo: "UniFi U6 Pro",
+        precio: 300
+    },
+    {
+        tipo: "Modem",
+        marca: "Netgear",
+        modelo: "CM1000",
+        precio: 250
+    }
+];
